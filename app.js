ef61bfdda953952ec0dbd316d91d2cd8240f1b1f
Sreehari <sreehari26012000.com>
2023-01-24 17:11:06 +0530

    database

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose =require("mongoose")
 const date = require(__dirname + "/date.js") 
 const app = express();
 let items = ["Project" , "Grocery Shopping"];
@@ -11,31 +12,68 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 app.use(express.static("public"))
 
+mongoose.connect("mongodb://localhost:27017/todolistDB",()=>{
+   console.log("MongoDb connected"); 
+});
+
+const itemsSchema = {
+    name:String
+}
+
+const Item = mongoose.model("Item", itemsSchema);
+
+
+const item1 = new Item ({
+    name:"Welcome to TO-DO-list"
+})
+
+
+const defaultItems = [item1];
+
 
 app.get("/", function (req, res) {
-    res.sendFile(__dirname + "/index.html")
-
+  Item.find({}, function (err,foundItems){
+ if (foundItems.length ===0) {
+Item.insertMany(defaultItems , function (err){
+    if(err) {
+        console.log(err);
+    } else {
+        console.log("inserted");
+    }
+})
+res.redirect("/")
+ }else{
+    res.render("list", {listTitle: day,newListItems:foundItems
+    }); 
+    
+ }
+  })
       let day = date.getDate()
-
-    res.render("list", {listTitle: day,newListItems:items
-    });
 });
 
 app.post("/",function(req,res){
    
-    let item = req.body.newItem; 
-    if (req.body.list === "Work") {
-       workItems.push(item);
-       res.redirect("/work")
-    }else {
-    items.push(item)
+    const  itemName = req.body.newItem; 
+
+    const item = new Item({
+        name:itemName
+    });
+    item.save()
     res.redirect("/")
-    } 
 });
 
- app.get("/work", function (req,res){
-    res.render("list", {listTitle:"Work List",newListItems:workItems});
- })
+app.post("/delete" , function(req,res){
+    const checkedItemId= req.body.checkbox
+
+    Item.findByIdAndRemove(checkedItemId, function(err){
+        if(!err) {
+            console.log("deleted success");
+            res.redirect("/")
+        }
+    })
+})
+
+ 
 
 app.post("/work", function(req,res){
     let item = req.body.newItem
